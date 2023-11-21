0a0e6dec19373f2b86ed9a20cc16d76dd912627d
Edit-Mr <elvismao.070512@gmail.com>
2023-11-21 11:56:12 +0800

    add key

--- get_data.js
+++ get_data.js
@@ -5,8 +5,8 @@ const fs = require("fs");
 
 async function getSubscriberCount() {
     try {
-        const apiKey = "YOUR_API_KEY"; // Replace with your actual API key
-        const channelId = "YOUR_CHANNEL_ID"; // Replace with your actual channel ID
+        const apiKey = "AIzaSyCD0u5WKys4mkmatSVwVxWA0SXJpzAKRqc";
+        const channelId = "UCHhpXyvjFxkKg2WYGKcvxMg";
 
         // Fetch channel statistics
         const response = await axios.get(
@@ -20,7 +20,8 @@ async function getSubscriberCount() {
         const ceylanResponse = await axios.get(
             `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCVjmBo22Mimyc4NR4Nz89MA&key=${apiKey}`
         );
-        const ceylanSub = ceylanResponse.data.items[0].statistics.subscriberCount;
+        const ceylanSub =
+            ceylanResponse.data.items[0].statistics.subscriberCount;
 
         // Fetch the most-viewed video
         const mostViewResponse = await axios.get(
@@ -30,7 +31,8 @@ async function getSubscriberCount() {
             title: mostViewResponse.data.items[0].snippet.title,
             videoId: mostViewResponse.data.items[0].id.videoId,
             description: mostViewResponse.data.items[0].snippet.description,
-            thumbnail: mostViewResponse.data.items[0].snippet.thumbnails.high.url,
+            thumbnail:
+                mostViewResponse.data.items[0].snippet.thumbnails.high.url,
             releaseDate: mostViewResponse.data.items[0].snippet.publishedAt,
         };
 
@@ -42,7 +44,8 @@ async function getSubscriberCount() {
             title: latestVideoResponse.data.items[0].snippet.title,
             videoId: latestVideoResponse.data.items[0].id.videoId,
             description: latestVideoResponse.data.items[0].snippet.description,
-            thumbnail: latestVideoResponse.data.items[0].snippet.thumbnails.high.url,
+            thumbnail:
+                latestVideoResponse.data.items[0].snippet.thumbnails.high.url,
             releaseDate: latestVideoResponse.data.items[0].snippet.publishedAt,
         };
 
@@ -52,7 +55,7 @@ async function getSubscriberCount() {
         );
         const videos = videosResponse.data.items;
         let totalWatchHours = 0;
-        videos.forEach((video) => {
+        videos.forEach(video => {
             const duration = video.contentDetails.duration;
             const hours = parseInt(duration.match(/(\d+)H/)?.[1]) || 0;
             const minutes = parseInt(duration.match(/(\d+)M/)?.[1]) || 0;
